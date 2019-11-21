4f48ac4a100c9ad308507f1652aee32c901c81bb
vlasisPi <vlassisp89@gmail.com>
2019-11-21 21:43:48 +0200

    Fix

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 # Transform scripts to Unix format
 find /scripts/ -iname "*.sh" | xargs sed -i -e 's/\r$//'
-sudo apt-get install --install-recommends linux-generic-lts-xenial
+sudo apt-get update -q
+sudo apt-get install -y --install-recommends linux-generic-lts-xenial
